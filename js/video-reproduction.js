45fb7b9a34102cfa555e7259bed48c4af00f344c
espinozaje <jeisson12aaron@gmail.com>
2025-07-16 13:47:09 -0500

    video fix safari

--- js/video-reproduction.js
+++ js/video-reproduction.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const video = document.querySelector(".gif-centro video");
+  const video = document.getElementById("miVideo");
 
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          video.play();
-        } else {
-          video.pause();
-        }
-      });
-    }, {
-      threshold: 0.1 
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        video.muted = false;
+        video.play().catch(err => console.log(err));
+      } else {
+        video.pause();
+      }
     });
+  }, { threshold: 0.1 });
 
-    observer.observe(video);
-  });
+  observer.observe(video);
+});
