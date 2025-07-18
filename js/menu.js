615eebcfab5c5ed1376c3b2542c7f98e6d13b5f2
espinozaje <jeisson12aaron@gmail.com>
2025-07-18 15:26:12 -0500

    menu-animation

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,21 @@
 const menuToggle = document.querySelector('.menu-toggle');
-  const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar');
 
-  menuToggle.addEventListener('click', () => {
-    navbar.classList.toggle('active');
-  });
+
+menuToggle.addEventListener('click', (event) => {
+  event.stopPropagation(); 
+  navbar.classList.toggle('active');
+});
+
+document.addEventListener('click', (event) => {
+  if (navbar.classList.contains('active') && !navbar.contains(event.target)) {
+    navbar.classList.remove('active');
+  }
+});
+
+
+window.addEventListener('scroll', () => {
+  if (navbar.classList.contains('active')) {
+    navbar.classList.remove('active');
+  }
+});
